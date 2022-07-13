1722b9fa76ef1e9ad6a404095a325565c7c48eec
Azur <azur@turquoise.dog>
2022-07-13 12:43:53 -0400

    Devise view polish & Turbo workaround

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import ToastController from "./toast_controller.js"
+application.register("toast", ToastController)
